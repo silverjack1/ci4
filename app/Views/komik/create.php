c45cce38fe2c41d75f6336ebf50a45adceb13bf8
silverjack1 <dian.nafi1@gmail.com>
2021-11-24 14:44:27 +0700

    kelima

--- app/Views/komik/create.php
+++ app/Views/komik/create.php
@@ -22,6 +22,7 @@
 <section class="content">
   <div class="container-fluid">
     <!-- awal container -->
+
     <div class="row">
       <div class="col-sm-6">
         <div class="card">
@@ -31,25 +32,30 @@
               <div class="form-group row">
                 <label for="judul" class="col-sm-3 col-form-label">Judul</label>
                 <div class="col-sm-9">
-                  <input type="text" class="form-control" id="judul" name="judul" name="judul" placeholder="Judul komik" autofocus>
+                  <input type="text" class="form-control <?= ($validation->hasError('judul'))
+                                                            ? 'is-invalid' : '' ?>" id="judul" name="judul" name="judul" placeholder="Judul komik" autofocus autocomplete="off">
+                  <div id="validationServer03Feedback" class="invalid-feedback">
+                    <?= $validation->getError('judul') ?>
+
+                  </div>
                 </div>
               </div>
               <div class="form-group row">
                 <label for="penulis" class="col-sm-3 col-form-label">Penulis</label>
                 <div class="col-sm-9">
-                  <input type="text" class="form-control" id="penulis" name="penulis" placeholder="Nama penulis">
+                  <input type="text" class="form-control" id="penulis" name="penulis" placeholder="Nama penulis" autocomplete="off">
                 </div>
               </div>
               <div class="form-group row">
                 <label for="penerbit" class="col-sm-3 col-form-label">Penerbit</label>
                 <div class="col-sm-9">
-                  <input type="text" class="form-control" id="penerbit" name="penerbit" placeholder="Penerbit">
+                  <input type="text" class="form-control" id="penerbit" name="penerbit" placeholder="Penerbit" autocomplete="off">
                 </div>
               </div>
               <div class="form-group row">
                 <label for="sampul" class="col-sm-3 col-form-label">Sampul</label>
                 <div class="col-sm-9">
-                  <input type="text" class="form-control" id="sampul" name="sampul" placeholder="Sampul">
+                  <input type="text" class="form-control" id="sampul" name="sampul" placeholder="Sampul" autocomplete="off">
                 </div>
               </div>
               <button class="btn btn-primary" type="submit">Submit form</button>
